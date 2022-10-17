076f3486b48ef42b09e74485ac91b607f5d3e7f5
RahmeniRaoui <rahmeniraoui@gmail.com>
2022-10-17 17:57:29 +0200

    finished

---
+++ components/Spinner.jsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import React from 'react'
+import spinner from "../public/loading-up.gif"
+
+const Spinner = () => {
+    return (
+        <div><Image className='w-[200px] m-auto block' src={spinner} alt='loading...' /></div>
+    )
+}
+
+export default Spinner
